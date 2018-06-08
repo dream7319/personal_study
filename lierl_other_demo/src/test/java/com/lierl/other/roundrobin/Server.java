f17175fa87d9048ad14c20bce9fdd6dbad4f0e70
lierlei <lierlei@xingyoucai.com>
2018-06-08 18:26:19 +0800

    add

---
+++ lierl_other_demo/src/test/java/com/lierl/other/roundrobin/Server.java
@@ -0,0 +1,19 @@
+package com.lierl.other.roundrobin;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author lierlei@xingyoucai.com
+ * @create 2018-06-07 11:16
+ **/
+@Data
+@ToString
+public class Server {
+	private String ip;
+	private int weight;
+
+	public Server(String ip) {
+		this.ip = ip;
+	}
+}
